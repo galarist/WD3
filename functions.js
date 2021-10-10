78bf88b20b83018638b4aa0699a98b522afcace4
galarist <galamboscristovao@gmail.com>
2021-10-11 05:29:21 +1000

    Make it more look better and make the regex working

--- functions.js
+++ functions.js
@@ -5,22 +5,39 @@ function validation() {
     var fName = document.getElementById("fname").value;
     var lName = document.getElementById("lname").value;
     var email = document.getElementById("email").value;
+    var vEmail = document.getElementById("vEmail").value;
     var pw1 = document.getElementById("password").value;
     var pw2 = document.getElementById("vPassword").value;
+    var fNameMsg = document.getElementById("errorFname");
+    var lNameMsg = document.getElementById("errorLname");
     var emailMsg = document.getElementById("errorEmail");
+    var emailVMsg = document.getElementById("errorVemail");
     var checkPW = document.getElementById("checkPW");
     var verifyPassword = document.getElementById("verifyPassword");
 
-    if (nameRegex.test(fName) !== true) {
-        //emailMsg.innerHTML = "Not correct email";
+    if (nameRegex.test(fName) !== true && fName.length > 0) {
+        fNameMsg.innerHTML = "Not correct name";
     } else {
-        //emailMsg.innerHTML = "";
+        fNameMsg.innerHTML = "";
     }
-
-    if (emailRegex.test(email) !== true) {
+    if (nameRegex.test(lName) !== true && lName.length > 0) {
+        lNameMsg.innerHTML = "Not correct name";
+    } else {
+        lNameMsg.innerHTML = "";
+    }
+    if (emailRegex.test(email) !== true && email.length > 0) {
         emailMsg.innerHTML = "Not correct email";
     } else {
         emailMsg.innerHTML = "";
+        if (vEmail.length !== 0) {
+            if (email !== vEmail) {
+                emailVMsg.innerHTML = "Email doesn't match";
+            } else {
+                emailVMsg.innerHTML = "Email match";
+            }
+        } else {
+            emailVMsg.innerHTML = "";
+        }
     }
 
     if (pwRegex.test(pw1) !== true && pw1.length > 0) {
